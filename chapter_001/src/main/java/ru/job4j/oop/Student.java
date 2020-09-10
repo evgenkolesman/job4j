dfe8813762e8713bd0cf3730b5cd392fb0d3cd16
evgenkolesman <70563563+evgenkolesman@users.noreply.github.com>
2020-09-10 13:30:57 +0400

    Add files via upload

---
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
+    }
+    public static void main(String[] args) {
+        Student petya = new Student();
+        String song = "I believe, I can fly";
+        petya.music(song);
+    }
+   }
